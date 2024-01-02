85954fcec841597c7718d5b6893372ef165f0568
Abdul Sabur <alsabur20@gmail.com>
2024-01-02 23:08:13 +0500

    manual mode added

--- FPCoal/FPCoal/main.asm
+++ FPCoal/FPCoal/main.asm
@@ -5,17 +5,22 @@
 
 .def A = r16
 .def AH = r17
+.def mode = r18
+.def alarmStatus = r19
+.def serialInput = r20
 
-.org 0x00
+
+.org 0x0000
+	
 	; I/O Pins Configuration
-	SBI DDRB, 5
-	SBI DDRB, 4
-	SBI DDRB, 3
+	SBI DDRB, 5		;green
+	SBI DDRB, 4		;red
+	SBI DDRB, 3		;alarm
 
 	SBI PORTB, 5 
 	CBI PORTB, 4
 	CBI PORTB, 3
-
+	
 	; ADC Configuration
 	LDI A,0b11000111 ; [ADEN ADSC ADATE ADIF ADIE ADIE ADPS2 ADPS1 ADPS0]
 	STS ADCSRA,A
@@ -25,9 +30,68 @@
 
 	; macro to initilize the serial communication
 	Serial_begin
+	ldi mode,'0'
+	ldi alarmStatus,'0'
 
 loop:
-    ; Start an ADC conversion by setting ADSC bit (bit 6)
+	ldi A, '0'
+	Serial_read
+	mov serialInput, r16
+
+	cpi serialInput, '0'
+	breq setAutoMode
+
+	cpi serialInput, '1'
+	breq setManualMode
+
+	cpi serialInput, '2'
+	breq alarmOn
+
+	cpi serialInput, '3'
+	breq alarmOff
+
+	cpi mode,'0'
+	breq automatic
+
+	cpi mode, '1'
+	breq manual
+
+	cpi mode, '2'
+	breq alarmOn
+
+	cpi mode, '3'
+	breq alarmOff
+
+	rjmp automatic
+
+setAutoMode:
+	ldi mode, '0'  ; '1' represents automatic mode
+	rjmp loop
+
+setManualMode:
+	ldi mode, '1'  ; '0' represents manual mode
+	rjmp loop
+
+manual:	
+	delay 1000
+	rjmp loop
+
+alarmOn:
+	ldi mode, '2'
+	CBI PORTB, 5	
+	SBI PORTB, 4	
+	SBI PORTB, 3
+	rjmp loop
+
+alarmOff:
+	ldi mode, '3'
+	SBI PORTB, 5
+	CBI PORTB, 4
+	CBI PORTB, 3
+    rjmp loop
+
+automatic:
+	; Start an ADC conversion by setting ADSC bit (bit 6)
     LDS A,ADCSRA ; Start Analog to Digital Conversion
 	ORI A,(1<<ADSC)
 	STS ADCSRA,A
@@ -38,22 +102,22 @@ adc_wait:
     rjmp adc_wait
     
 	; Read ADC result
-    LDS r18, ADCL
-	LDS r18, ADCH	
-	Serial_writeReg_ASCII r18
-	Serial_writeNewLine
+    LDS A, ADCL
+	LDS A, ADCH	
+	Serial_writeReg A
 	delay 1000
     ; Compare ADC result with TRIGPOINT
-    cpi r18, 200
+    cpi A, 190
     brsh smokeDetected
-    
     SBI PORTB, 5
 	CBI PORTB, 4
 	CBI PORTB, 3
     rjmp loop
-    
+
 smokeDetected:
-    CBI PORTB, 5
-	SBI PORTB, 4
+	CBI PORTB, 5	
+	SBI PORTB, 4	
 	SBI PORTB, 3
-    rjmp loop
+	rjmp loop
+
+	
